d2b11a3d90f8aa0f7d6d3d3a6b80889d9ad488cd
gwen041 <almiratabios01@gmail.com>
2026-06-09 21:42:49 +0800

    Add: button functions

--- script.js
+++ script.js
@@ -1,36 +1,85 @@
 let total;
-
 let input = document.getElementById('input-field');
-let operands = document.querySelector('#btn-components');
-let initialValue = 0;
 
+function btnOne() {
+  const one = document.getElementById('one');
+  input.value += one.textContent; 
+}
 
-operands.addEventListener('click', (event) => {
+function btnTwo() {
+  const two = document.getElementById('two');
+  input.value += two.textContent; 
+}
 
-  let targetNum = event.target;
+function btnThree() {
+  const three = document.getElementById('three');
+  input.value += three.textContent; 
+}
 
-  if (targetNum.classList.contains('btn-style')) {
+function btnFour() {
+  const four = document.getElementById('four');
+  input.value += four.textContent; 
+}
 
-    let key = targetNum.textContent;
-    
-    //input.value += targetNum.textContent;
-    // if (input.value === '' || input.value === null) {
-    //   input.value = targetNum.textContent;
-    // //console.log(targetNum.textContent);
-    //   return;
-    // }
+function btnFive() {
+  const five = document.getElementById('five');
+  input.value += five.textContent; 
+}
 
-    if (key == 'AC') {
-      input.value = '';
-      return;
-    }
+function btnSix() {
+  const six = document.getElementById('six');
+  input.value += six.textContent; 
+}
 
-    if (key == 'DEL') {
-      input.value = input.value.slice(0, -1);
-      return;
-    }
+function btnSeven() {
+  const seven = document.getElementById('seven');
+  input.value += seven.textContent; 
+}
 
-    input.value += key;
+function btnEight() {
+  const eight = document.getElementById('eight');
+  input.value += eight.textContent; 
+}
 
+function btnNine() {
+  const nine = document.getElementById('nine');
+  input.value += nine.textContent; 
+}
+
+function btnZero() {
+  const zero = document.getElementById('zero');
+  input.value += zero.textContent; 
+}
+
+function btnPoint() {
+  const point = document.getElementById('point');
+  input.value += point.textContent;
+}
+
+function btnDel() {
+  let val = String(input.value);
+  let newVal = '';
+
+  for (let i = 0; i < val.length - 1; i++) {
+    newVal += val[i];
   }
-});
+  input.value = newVal;
+}
+
+function clearAll() {
+  input.value = '';
+}
+
+function add() { input.value += "+"; }
+function minus() { input.value += '-'; }
+function multiply() { input.value += 'x'; }
+function divide() { input.value += '/'; }
+
+function compute() {
+  let nums = String(input.value);
+  
+  if (input.value != 'null' || input.value != '') {
+    total = eval(nums.replace('x', '*'));
+    input.value = total;
+  }
+}
